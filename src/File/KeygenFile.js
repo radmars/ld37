85eabb4f4b7ea700dd53a047ec3e027886140ece
Eugene Marcotte <emarcotte@gmail.com>
2016-12-11 13:24:40 -0500

    keygens are the best

--- src/File/KeygenFile.js
+++ src/File/KeygenFile.js
@@ -6,7 +6,23 @@ class KeygenFile extends File {
 	}
 
 	launch() {
-		alert("all the keys");
+		// TODO if we wanted to be nice we'd cache audio tags but I DNGAF.
+		jQuery(
+			"<span>" +
+			"<img src='img/keygen_1.png' />" +
+			"<audio autoplay loop>" +
+			"<source src='music/ld37-2.mp3' type='audio/mp3' />" +
+			"</audio>" +
+			"</span>"
+		).dialog({
+			autoOpen: true,
+			beforeClose: function(e) {
+				jQuery("audio", this)[0].pause();
+			},
+			title: this.name,
+			width: "auto",
+			appendTo: $(".desktop-area"),
+		});
 	}
 
 	static generateName() {
