54c6051ad7c5896158bc228f55f98671ada0390b
Eugene Marcotte <emarcotte@gmail.com>
2016-12-11 17:30:09 -0500

    more keygens

--- src/File/KeygenFile.js
+++ src/File/KeygenFile.js
@@ -1,15 +1,16 @@
 "use strict"
 
 class KeygenFile extends File {
-	constructor(name) {
+	constructor(name, image) {
 		super(name + ".exe", "img/key_icon.png");
+		this.keyGenImage = image;
 	}
 
 	launch() {
 		// TODO if we wanted to be nice we'd cache audio tags but I DNGAF.
 		jQuery(
 			"<span>" +
-			"<img src='img/keygen_1.png' />" +
+			"<img src='" + this.keyGenImage + "' />" +
 			"<audio autoplay loop>" +
 			"<source src='music/ld37-2.mp3' type='audio/mp3' />" +
 			"</audio>" +
@@ -30,9 +31,19 @@ class KeygenFile extends File {
 		return "KEYGEN-" + base;
 	}
 
+	static generateImage() {
+		return [
+			'img/keygen1.png',
+			'img/keygen2.png',
+			'img/keygen3.png',
+			'img/keygen4.png',
+		].randomElement()
+	}
+
 	static generate() {
 		var name = KeygenFile.generateName();
-		return new KeygenFile(name);
+		var image = KeygenFile.generateImage();
+		return new KeygenFile(name,image);
 	}
 }
 
