26459f17ce8b8f057221158e1bfab3a190da1a0f
Adhesion <adhesionmusic@gmail.com>
2016-12-11 23:05:35 -0500

    Keygen plays musics

--- src/File/KeygenFile.js
+++ src/File/KeygenFile.js
@@ -7,12 +7,16 @@ class KeygenFile extends File {
 	}
 
 	launch() {
+		var music = this.keyGenImage.slice(this.keyGenImage.indexOf('/')+1, this.keyGenImage.lastIndexOf('.'));
+		music = "music/ld37-" + music;
+
 		// TODO if we wanted to be nice we'd cache audio tags but I DNGAF.
 		jQuery(
 			"<span>" +
 			"<img src='" + this.keyGenImage + "' />" +
 			"<audio autoplay loop>" +
-			"<source src='music/ld37-2.mp3' type='audio/mp3' />" +
+			"<source src='" + music + ".mp3' type='audio/mp3' />" +
+			"<source src='" + music + ".ogg' type='audio/ogg' />" +
 			"</audio>" +
 			"</span>"
 		).dialog({
@@ -37,6 +41,7 @@ class KeygenFile extends File {
 			'img/keygen2.png',
 			'img/keygen3.png',
 			'img/keygen4.png',
+			'img/keygendde.png',
 		].randomElement()
 	}
 
