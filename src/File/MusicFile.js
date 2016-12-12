649d47bd061c056df41857df20b286289a409067
Adhesion <adhesionmusic@gmail.com>
2016-12-11 23:40:41 -0500

    Add music files

--- src/File/MusicFile.js
+++ src/File/MusicFile.js
@@ -3,19 +3,42 @@
 class MusicFile extends File {
 	constructor(name) {
 		super(name + ".mp3", "img/cd_icon.png");
+
+		this.filenames = [
+			"atmo-dnb",
+			"tup",
+			"aor",
+		];
 	}
 
 	launch() {
-		alert("Sick AMV music here");
+		var idx = this.name.charAt(0) - 1;
+		var music = "music/ld37-" + this.filenames[idx];
+
+		jQuery(
+			"<span>" +
+			"<img src='img/music-player.png' />" +
+			"<audio autoplay loop>" +
+			"<source src='" + music + ".mp3' type='audio/mp3' />" +
+			"<source src='" + music + ".ogg' type='audio/ogg' />" +
+			"</audio>" +
+			"</span>"
+		).dialog({
+			autoOpen: true,
+			beforeClose: function(e) {
+				jQuery("audio", this)[0].pause();
+			},
+			title: this.name,
+			width: "auto",
+			appendTo: $(".desktop-area"),
+		});
 	}
 
 	static generateName() {
 		return [
-			'Metaldozer - Ride the Pony',
-			'Metaldozer - Commander of Dancers',
-			'Tifany Spikes - I\'m Happy Girl',
-			'Angry at the device - Baboon Radio',
-			'Angry at the device - Geese on Display',
+			'1.Adhesion-rojan-dnb-mix-2k2(edit)',
+			'2.The Uncertainty Principle-OST_GAMERIP',
+			'3.ATTACK_ON_RADMARS-OST_GAMERIP',
 		].randomElement();
 	}
 
